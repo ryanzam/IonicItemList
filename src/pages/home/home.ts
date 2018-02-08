e71d049af880d42a044274a86a5fd12fa7a8a5ff
ryanzam <ryanzam2005@yahoo.com>
2018-02-08 20:05:12 +0200

    first mobile app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+
+
+
+@IonicPage()
 
 @Component({
   selector: 'page-home',
